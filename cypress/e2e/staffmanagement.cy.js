e6e75334f6beffe1f0e7667cc23dcabbde1259ac
danishjai <danishjailany@gmail.com>
2024-08-18 14:25:31 +0800

    Added all relevant test cases

--- cypress/e2e/staffmanagement.cy.js
+++ cypress/e2e/staffmanagement.cy.js
@@ -1,5 +1,174 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import staff from '../fixtures/staff.json'
+import accounts from '../fixtures/accounts.json'
+import elements from '../fixtures/elements.json'
+
+describe('Staff Management', () => {
+  beforeEach(() => {
+    //Navigate to Qashier website and login to account
+    cy.loginToAccount(accounts.username, accounts.password);
+
+    //Navigate to the Staff Management Dashboard
+    cy.navigateToStaffManagement();
+  });
+  
+  describe('Add Staff Feature', () => {
+    it('User is able to add a new staff successfully', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Delete the newly created staff
+      cy.deleteStaff();
+    });
+
+    it('User is shown an error when creating a new staff without staff name', () => {
+      //Create a new staff with empty name
+      cy.createNewStaff(staff.invalidStaffName, staff.newStaffRate, staff.newStaffPIN);
+
+      //Verify error popup is shown
+      cy.get('div').contains('Please type something');
+    });
+  
+    it('User is not able to create a new staff with hourly rate less than 0.01', () => {
+      //Create a new staff with invalid staff hourly rate
+      cy.createNewStaff(staff.newStaffName, staff.invalidStaffRate, staff.newStaffPIN);
+
+      //Close the staff form popup
+      cy.get('i').contains('close').click();
+
+      //Verify the staff was not created with an invalid hourly rate
+      cy.get('div').contains('No data available');
+    });
+
+    it('User is shown an error when creating a new staff with invalid staff pin', () => {
+      //Create a new staff with invalid staff PIN
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.invalidStaffPIN);
+
+      //Verify error popup is shown
+      cy.get('div').contains('PIN must contain exactly 4 digits');
+    });
+
+    it('User is able to cancel the action to create a new staff', () => {
+      //Click on Add Staff button
+      cy.get(elements.addStaffButton).click();
+
+      //Close the staff form popup
+      cy.get('i').contains('close').click();
+    });
+  });
+
+  describe('Edit Staff Feature', () => {
+    afterEach(() => {
+      //Delete the updated staff
+      cy.deleteStaff();
+    });
+    it('User is able to update an existing staff successfully', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Update newly created staff details
+      cy.updateExistingStaff(staff.updatedStaffName, staff.updatedStaffRate, staff.updatedStaffPIN);
+
+      //Verify updated staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.updatedStaffName, staff.updatedStaffRate, staff.updatedStaffPIN);
+    });
+
+    it('User is shown an error when updating an existing staff without staff name', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+
+      //Update newly created staff details with empty name
+      cy.updateExistingStaff(staff.invalidStaffName, staff.updatedStaffRate, staff.updatedStaffPIN);
+
+      //Verify error popup is shown
+      cy.get('div').contains('Please type something');
+
+      //Close staff form popup
+      cy.get('i').contains('close').click();
+    });
+
+    it('User is not able to update an existing staff with hourly rate less than 0.01', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+
+      //Update newly created staff details with invalid hourly rate
+      cy.updateExistingStaff(staff.updatedStaffName, staff.invalidStaffRate, staff.updatedStaffPIN);
+
+      //Close staff form popup
+      cy.get('i').contains('close').click();
+
+      //Verify staff details are NOT updated
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+    });
+
+    it('User is shown an error when updating an existing staff with invalid staff pin', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+
+      //Update newly created staff details with invalid staff PIN
+      cy.updateExistingStaff(staff.updatedStaffName, staff.updatedStaffRate, staff.invalidStaffPIN);
+
+      //Verify error popup is shown
+      cy.get('div').contains('PIN must contain exactly 4 digits');
+
+      //Close staff form popup
+      cy.get('i').contains('close').click();
+    });
+
+    it('User is able to cancel the action to update an existing staff', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+    
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+
+      //Click on listed staff on dashboard
+      cy.get('tbody').click();
+
+      //Close the staff form popup
+      cy.get('i').contains('close').click();
+    });
+  });
+
+  describe('Delete Staff Feature', () => {
+    it('User is able to delete an existing staff successfully', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Delete created staff
+      cy.deleteStaff();
+    });
+
+    it('User is able to cancel the action to delete an existing staff', () => {
+      //Create a new staff with valid details
+      cy.createNewStaff(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+      
+      //Verify created staff details on Staff Management Dashboard
+      cy.verifyStaffDetails(staff.newStaffName, staff.newStaffRate, staff.newStaffPIN);
+
+      //Click on existing staff and delete
+      cy.get('tbody').click();
+      cy.get('span').contains('Delete').click();
+
+      //Click on Cancel button in confirmation popup
+      cy.get('span').contains('Cancel').click();
+    });
+  });
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,20 +1,79 @@
-// ***********************************************************
-// This example support/e2e.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands'
+import elements from '../fixtures/elements.json'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.Commands.add('loginToAccount', (username, password) => {
+  //Open Qashier website
+  cy.visit('https://hq.qashier.com/#/login');
+
+  //Enter username & password
+  cy.get('input[type="text"]').type(username);
+  cy.get('input[type="password"]').type(password);
+  
+  //Log into account
+  cy.get('span').contains('Login').click();
+});
+
+Cypress.Commands.add('navigateToStaffManagement', () => {
+  //Navigate to the Staff Management page
+  cy.get(elements.staffManagementTab).contains('Staff Management').click();
+  cy.get(elements.staffManagementOption).click();
+});
+
+Cypress.Commands.add('createNewStaff', (staffName, staffRate, staffPIN) => {
+  //Click on Add Staff button
+  cy.get(elements.addStaffButton).click();
+  
+  //Fill in staff details
+  cy.fillInDetails(staffName, staffRate, staffPIN)
+});
+
+Cypress.Commands.add('updateExistingStaff', (staffName, staffRate, staffPIN) => {
+  //Click on listed staff on dashboard
+  cy.get('tbody').click();
+  
+  //Fill in staff details
+  cy.fillInDetails(staffName, staffRate, staffPIN);
+});
+
+Cypress.Commands.add('fillInDetails', (staffName, staffRate, staffPIN) => {
+  //Enter staff name
+  cy.contains(elements.staffDetailsInput, "Name")
+    .parent()
+    .find('input')
+    .clear()
+    .type(staffName, { force: true });
+  
+  //Enter staff hourly rate
+  cy.contains(elements.staffDetailsInput, "Hourly Rate ()")
+    .parent()
+    .find('input')
+    .clear()
+    .type(staffRate, { force: true });
+
+  //Enter staff PIN
+  cy.contains(elements.staffDetailsInput, "Staff PIN")
+    .parent()
+    .find('input')
+    .clear()
+    .type(staffPIN, { force: true });
+  
+  //Create/Update staff
+  cy.get('span').contains('Confirm').click();
+});
+
+Cypress.Commands.add('verifyStaffDetails', (staffName, staffRate, staffPIN) => {
+  //Verify staff name, rate & PIN on management dashboard
+  cy.get('td').contains(staffName);
+  cy.get('td').contains(staffRate);
+  cy.get('td').contains(staffPIN);
+});
+
+Cypress.Commands.add('deleteStaff', () => {
+  //Click on existing staff and delete
+  cy.get('tbody').click();
+  cy.get('span').contains('Delete').click();
+  cy.get(elements.deleteConfirmationButton).last().click();
+
+  //Verify staff has been deleted
+  cy.get('div').contains('No data available');
+});
